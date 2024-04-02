ec57f00c0ccd14505fed0fce94808b271f012aa2
Bernard Teles <bernardtelgon@gmail.com>
2024-04-01 23:39:56 -0300

    first day

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+class PagesController < ApplicationController
+  def contact
+    # This action will render the app/views/pages/contact.html.erb template
+  end
+
+  def about
+    # This action will render the app/views/pages/about.html.erb template
+
+  end
+
+  def home
+    # This action will render the app/views/pages/home.html.erb template
+
+  end
+end
